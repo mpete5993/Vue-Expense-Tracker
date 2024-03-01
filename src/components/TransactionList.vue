ca678b46f0f1bdae94a33cc9f1380e2da3d7e56f
Bataung Mpete <53331473+mpete5993@users.noreply.github.com>
2024-03-01 15:28:01 +0200

    Update Transaction List items

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -4,7 +4,7 @@
 		<li v-for="transaction in transactions" 
 			:key="transaction.id"
 			:class="transaction.amount < 0 ? 'minus' : 'plus'">
-			{{transaction.text}} <span>${{transaction.amount}}</span>
+			{{transaction.text}} <span>R{{transaction.amount}}</span>
 			<button class="delete-btn" @click="deleteTransaction(transaction.id)">X</button>
 		</li>
 	</ul>
